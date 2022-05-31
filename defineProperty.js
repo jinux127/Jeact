f3bc3f22212852637412af1e33db711fec720ca4
jinwoo <wwo1278@gmail.com>
2022-06-01 00:48:19 +0900

    함수화 해서 사용

--- defineProperty.js
+++ defineProperty.js
@@ -1,39 +1,38 @@
 let currentObserver = null;
 
-const state = {
-  a: 10,
-  b: 20,
+const observe = (fn) => {
+  currentObserver = fn;
+  fn();
+  currentObserver = null;
 };
 
-const stateKeys = Object.keys(state);
-const observer = () => console.log(`a+b = ${state.a + state.b}`);
+const observable = (obj) => {
+  Object.keys(obj).forEach((key) => {
+    let _value = obj[key];
+    const observers = new Set();
 
-for (const key of stateKeys) {
-  let _value = state[key];
-  const observers = new Set();
-  Object.defineProperty(state, key, {
-    get() {
-      if (currentObserver) observers.add(currentObserver);
-      return _value;
-    },
-    set(value) {
-      _value = value;
-      observers.forEach((observer) => observer());
-    },
+    Object.defineProperty(state, key, {
+      get() {
+        if (currentObserver) observers.add(currentObserver);
+        return _value;
+      },
+      set(value) {
+        _value = value;
+        observers.forEach((fn) => fn());
+      },
+    });
   });
-}
 
-const plus_calc = () => {
-  currentObserver = plus_calc;
-  console.log(`a+b = ${state.a + state.b}`);
-};
-const minus_calc = () => {
-  currentObserver = minus_calc;
-  console.log(`a-b = ${state.a - state.b}`);
+  return obj;
 };
 
-plus_calc();
-minus_calc();
+const state = observable({ a: 10, b: 20 });
+
+observe(() => console.log(`a = ${상태.a}`));
+observe(() => console.log(`b = ${상태.b}`));
+observe(() => console.log(`a + b = ${상태.a} + ${상태.b}`));
+observe(() => console.log(`a * b = ${상태.a} + ${상태.b}`));
+observe(() => console.log(`a - b = ${상태.a} + ${상태.b}`));
 
 state.a = 100;
 state.b = 200;
