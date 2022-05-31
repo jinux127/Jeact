d5e477d1f8dcbfe2c8543f3e40e366e9750b52c0
jinwoo <wwo1278@gmail.com>
2022-05-31 23:04:46 +0900

    여러개의 옵저버 등록

--- defineProperty.js
+++ defineProperty.js
@@ -1,3 +1,5 @@
+let currentObserver = null;
+
 const state = {
   a: 10,
   b: 20,
@@ -8,16 +10,30 @@ const observer = () => console.log(`a+b = ${state.a + state.b}`);
 
 for (const key of stateKeys) {
   let _value = state[key];
+  const observers = new Set();
   Object.defineProperty(state, key, {
     get() {
+      if (currentObserver) observers.add(currentObserver);
       return _value;
     },
     set(value) {
       _value = value;
-      observer();
+      observers.forEach((observer) => observer());
     },
   });
 }
+
+const plus_calc = () => {
+  currentObserver = plus_calc;
+  console.log(`a+b = ${state.a + state.b}`);
+};
+const minus_calc = () => {
+  currentObserver = minus_calc;
+  console.log(`a-b = ${state.a - state.b}`);
+};
+
+plus_calc();
+minus_calc();
 
 state.a = 100;
 state.b = 200;
